1ba9777fb04233a28db559b5c69c41b5eed7289e
Jessica Aboukasm <jessicaaboukasm@0587350667.wireless.umich.net>
2016-02-25 13:40:29 -0500

    actually just kidding didn't add the files my b

---
+++ eecs441/RoomEase/RoomEase/UserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserViewController.swift
+//  RoomEase
+//
+//  Created by Jessica Aboukasm on 2/25/16.
+//  Copyright (c) 2016 RoomEase - EECS 441. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
