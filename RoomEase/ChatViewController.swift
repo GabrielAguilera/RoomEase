902b6f2e1a1f7ffe66fdfc71db15c51994fbcbaa
Gabriel Aguilera <gabrieag@umich.edu>
2016-03-08 23:44:32 -0500

    Chat tab links to Facebook Messenger app.

--- RoomEase/ChatViewController.swift
+++ RoomEase/ChatViewController.swift
@@ -15,11 +15,19 @@ class ChatViewController: UIViewController  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       // [FBSDKMessageDialog showWithContent: delegate:nil];
 
-        // Do any additional setup after loading the view, typically from a nib.
+        let messageButton : UIButton = FBSDKMessengerShareButton.circularButtonWithStyle(FBSDKMessengerShareButtonStyle.Blue, width: 50)
+        messageButton.addTarget(self, action: "openMessenger", forControlEvents: UIControlEvents.TouchUpInside)
+        messageButton.frame = CGRect(x: self.view.frame.size.width/2 - 40, y: self.view.frame.size.height/2 - 40, width: 80, height: 80)
+        self.view.addSubview(messageButton)
     }
 
+    func openMessenger() {
+        // FYI, the url can break at anytime if fb feels like changing it
+        // fb-messenger://groupthreadfbid/%s could work in the future???
+        UIApplication.sharedApplication().openURL(NSURL(string: "fb-messenger://")!)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
