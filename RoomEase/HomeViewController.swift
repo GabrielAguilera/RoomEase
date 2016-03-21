052aa985bc346e3b0650858806d09569d4dec18a
Mitchell <mgild@umich.edu>
2016-03-21 00:44:50 -0400

    users on home screen sorted and pulled from firebase

--- RoomEase/HomeViewController.swift
+++ RoomEase/HomeViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
+import Firebase
 
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -73,15 +74,45 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let myCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
-        let sortedNames = retrieveRoommateRankings()
+        var sortedNames = [String]()
         let sortedTasks = retrieveTaskRankings()
         switch(homeSegmentedControl.selectedSegmentIndex)
         {
         case 0:
-            let pointValue = String(self.shareData.roommateRankings[sortedNames[indexPath.row]]!)
-            let cellText = pointValue + "  |   " + sortedNames[indexPath.row]
-            myCell.textLabel!.text = cellText
-            addTaskButton.hidden = true
+            let ref = Firebase(url: "https://fiery-heat-3695.firebaseio.com/users")
+            ref.observeEventType(.Value, withBlock: { snapshot in
+                var roomate_scores = [String:Int]()
+                //This loop builds the array from the Firebase snap
+                for item in snapshot.children {
+                    let user = item as! FDataSnapshot
+                    //get the username from the tuple
+                    let user_name = user.key
+                    //get the users points from the child value
+                    let user_points = Int(String(user.childSnapshotForPath("points").value))
+                    roomate_scores[user_name] = user_points
+                }
+                //function for sorting by value
+                let byValue = {
+                    (elem1:(key: String, val: Int), elem2:(key: String, val: Int))->Bool in
+                    if elem1.val > elem2.val {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+                //sorts roomates by value
+                let sorted_roomate_scores = roomate_scores.sort(byValue)
+                //converts dict to string array to be inserted into ist view
+                for (key, value) in sorted_roomate_scores {
+                    sortedNames.append("\(key)  |   \(value)")
+                }
+                
+                //inserts the strings into cells in listview
+                let cellText = sortedNames[indexPath.row]
+                myCell.textLabel!.text = cellText
+                self.addTaskButton.hidden = true
+            })
+
             break
         case 1:
             let pointValue = String(self.shareData.taskList[sortedTasks[indexPath.row]]!)
