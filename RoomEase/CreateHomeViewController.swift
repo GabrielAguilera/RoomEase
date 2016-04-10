cb2a5db4ee9aa2c2d8f95ffa5c3cb9e2a2640e06
Gabriel Aguilera <gabrieag@umich.edu>
2016-04-09 20:40:00 -0400

    Adding file for CreateHomeView.

---
+++ RoomEase/CreateHomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CreateHomeViewController.swift
+//  RoomEase
+//
+//  Created by Gabriel Aguilera on 4/9/16.
+//  Copyright © 2016 RoomEase - EECS 441. All rights reserved.
+//
+
+import UIKit
+
+class CreateHomeViewController : UIViewController {
+    
+}
