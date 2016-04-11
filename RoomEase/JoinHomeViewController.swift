3a9d0b554cccb4557ff7ac12f68adfdc09c9572b
Gabriel Aguilera <gabrieag@umich.edu>
2016-04-10 21:38:15 -0400

    Joining a house adds to the array obj.

--- RoomEase/JoinHomeViewController.swift
+++ RoomEase/JoinHomeViewController.swift
@@ -22,15 +22,36 @@ class JoinHomeViewController: UIViewController {
             self.presentViewController(alertController, animated: true, completion: nil)
         } else {
             joinHome()
-            self.performSegueWithIdentifier("HomeJoinedSegue", sender: nil)
         }
     }
     
     func joinHome()
     {
-        // We could check to see if the home exists before we update the user
-        let ref = Firebase(url: self.shareData.ROOT_URL + "users/" + self.shareData.currentUserId + "/homeId")
-        ref.setValue(homeIdTextBox.text!)
-        self.shareData.currentHomeId = homeIdTextBox.text!
+        let homeId = homeIdTextBox.text!
+        let homeRef = Firebase(url: self.shareData.ROOT_URL + "home/" + homeId)
+        homeRef.observeSingleEventOfType(FEventType.Value, withBlock: { homeObj in
+            if homeObj.value is NSNull {
+                let alertController = UIAlertController(title: "Home doesn't exist!", message: "Please enter a different home name to continue!", preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alertController, animated: true, completion: nil)
+            } else {
+                // Update user homeid
+                let userRef = Firebase(url: self.shareData.ROOT_URL + "users/" + self.shareData.currentUserId + "/homeId")
+                userRef.setValue(homeId)
+                self.shareData.currentHomeId = homeId
+                
+                // Update member array
+                let newMembers = homeObj.childSnapshotForPath("members").value as! NSMutableArray
+                let num = Int(self.shareData.currentUserId)
+                let myNum = NSNumber(integer:num!)
+                newMembers.addObject(myNum)
+                
+                // Push new member array
+                let homeRef = Firebase(url: self.shareData.ROOT_URL + "home/" + homeId + "/members/")
+                homeRef.setValue(newMembers)
+                
+                self.performSegueWithIdentifier("HomeJoinedSegue", sender: nil)
+            }
+        })
     }
 }
