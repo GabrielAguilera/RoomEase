57f239a4e8e617405e63a75811bcdc9efa9392a6
aboukasm@umich.edu <aboukasm@umich.edu>
2016-04-12 14:14:52 -0400

    Removed best roommate label for good

--- RoomEase/HomeViewController.swift
+++ RoomEase/HomeViewController.swift
@@ -16,7 +16,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var taskTableView: UITableView!
     @IBOutlet weak var homeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var addTaskButton: UIButton!
-    @IBOutlet weak var bestRoommateLabel: UILabel!
     @IBOutlet weak var welcomeHomeLabel: UILabel!
     
     let shareData = ShareData.sharedInstance
@@ -169,12 +168,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             let cellText = pointValue + "  |   " + sortedNames[indexPath.row]
             myCell.textLabel!.text = cellText
             addTaskButton.hidden = true
-            if(self.shareData.bestRoommate) {
-                bestRoommateLabel.hidden = false
-            }
-            else {
-                bestRoommateLabel.hidden = true
-            }
             break
             
         case 1:
@@ -182,7 +175,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             let cellText = pointValue + "  |   " + self.localTaskList[indexPath.row].title
             myCell.textLabel!.text = cellText
             addTaskButton.hidden = false
-            bestRoommateLabel.hidden = true
             break
             
         default:
